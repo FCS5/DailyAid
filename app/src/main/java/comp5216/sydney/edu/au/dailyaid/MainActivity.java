ff63710c7885d1095ac8d9df81fefd12b906c645
FCS5 <cjhs50000@gmail.com>
2022-10-18 16:12:45 +1100

    <feat>(<view>): <homepage layout tab>

--- app/src/main/java/comp5216/sydney/edu/au/dailyaid/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/dailyaid/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_homepage);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
     }
